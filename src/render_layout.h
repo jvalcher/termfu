ee1a109f9d88e40bc811e061d6c710198f36a42a
Jeff Valcher <jvalcher@gmail.com>
2024-07-26 13:23:39 -0500

    Updated comment

--- src/render_layout.h
+++ src/render_layout.h
@@ -9,10 +9,11 @@
 /*
    Render Ncurses layouts
    ----------
-   Calculates and renders Ncurses header and windows (no data)
-   based on configuration file settings set in parse_config_file()
+   - Calculates and renders Ncurses header and windows (no data)
+     based on configuration file settings set in parse_config_file()
+   - layout_title set in CONFIG_FILE (see data.h, parse_config_file.c)
 */
-void render_layout (char*, state_t*);
+void render_layout (char *layout_title, state_t *state);
 
 
 
