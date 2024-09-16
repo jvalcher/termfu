051b8b831f1f6076fdd70f0342e82cbaabc32c74
jvalcher <jvalcher@gmail.com>
2024-09-16 01:13:49 -0500

    Created

---
+++ tests/file_update.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "../src/utilities.h"
+
+int
+main (void)
+{
+    struct stat file_stat;
+    char *path = "file.txt";
+    FILE *fp;
+    time_t file_time;
+
+    putchar ('\n');
+
+    // no updates
+    fp = fopen (path, "w");
+    fprintf (fp, "Initial write\n");
+    fclose (fp);
+
+    if (stat (path, &file_stat) != 0) {
+        fprintf (stderr, "Unable to get status of file \"%s\"\n", path);
+    }
+    file_time = file_stat.st_mtim.tv_sec;
+
+    if (file_was_updated (file_time, path)) {
+        printf ("No update error\n");
+    } else {
+        printf ("No update success\n");
+    }
+
+    // update
+    usleep (2000000);
+    fp = fopen (path, "a");
+    fprintf (fp, "Initial write\n");
+    fclose (fp);
+
+    if (file_was_updated (file_time, path)) {
+        printf ("Update success\n");
+    } else {
+        printf ("Update error\n");
+    }
+
+    remove (path);
+
+    return 0;
+}
---
+++ tests/gdb_bin_path.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include "../src/data.h"
+#include "../src/plugins.h"
+#include "../src/start_debugger.h"
+#include "../src/update_window_data/get_binary_path.h"
+
+
+int
+main (void)
+{
+    state_t *state = (state_t*) malloc (sizeof (state_t));
+    set_num_plugins (state);
+    state->debugger = (debugger_t*) malloc (sizeof (debugger_t));
+    state->debugger->index = DEBUGGER_GDB;
+
+    char *cmd[] = {"gdb", "--quiet", "--interpreter=mi", "../misc/hello"};
+    state->command = cmd;
+
+    start_debugger (state);
+
+    get_binary_path (state);
+
+    printf ("\n%s\n", state->debugger->prog_path);
+
+    return 0;
+}
