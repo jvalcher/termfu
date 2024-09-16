bfbccc8ebc79ede1a41665def3acfaff88186bf7
jvalcher <jvalcher@gmail.com>
2024-09-16 16:00:03 -0500

    Edited comment

--- src/parse_config_file.h
+++ src/parse_config_file.h
@@ -7,9 +7,12 @@
 
 
 /*
-   Parse external configuration file (see CONFIG_FILE in data.h) located
-   in current or home directory
-*/  
+    Parse CONFIG_FILE data
+    --------------
+    - Allocate and add plugins to `plugin` *plugin_t array  (plugins.h)
+    - Allocate layout_t linked list, calculate window ratios, ...
+    - ...
+*/
 void parse_config_file (state_t *state);
 
 
