57ef803a1a341b41eeb44b89c755eea638d1c795
jvalcher <jvalcher@gmail.com>
2024-09-16 16:04:45 -0500

    Removed spaces

--- src/parse_debugger_output.h
+++ src/parse_debugger_output.h
@@ -11,11 +11,14 @@
 */
 void parse_debugger_output (state_t *state);
 
+
+
 /*
     Parse specific debugger output (for testing)
 */
 void parse_debugger_output_gdb (reader_t *reader);
 void parse_debugger_output_pdb (reader_t *reader);
+
 
 
 #endif
